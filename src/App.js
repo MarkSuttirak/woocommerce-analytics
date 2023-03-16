a03cd33f137353c7df0819b69bdf28f9c06a5b07
MarkSuttirak <suttirak.ch@zaviago.com>
2023-03-16 11:03:57 +0700

    Commit codes

--- src/App.js
+++ src/App.js
@@ -1,79 +1,89 @@
 import logo from './logo.svg';
 import './App.css';
-import { Typography, Select, Space, Card, Button } from 'antd';
+import { Typography, Select, Space, Card, Button, DatePicker } from 'antd';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+  const { RangePicker } = DatePicker;
   const { Title, Text, Paragraph } = Typography;
   return (
     <div className="App">
       <header className="App-header">
-        <Paragraph>Date range:</Paragraph>
-        <Space>
-        <Select
-          placeholder="Select a date range"
-          style={{
-            width: 300,
-          }}
-          options={[
-            {
-              value: 'today',
-              label: 'Today',
-            },
-            {
-              value: 'yesterday',
-              label: 'Yesterday',
-            },
-            {
-              value: 'week to date',
-              label: 'Week to date',
-            },
-            {
-              value: 'month to date',
-              label: 'Month to date',
-            },
-            {
-              value: 'quarter to date',
-              label: 'Quarter to date',
-            },
-            {
-              value: 'year to date',
-              label: 'Year to date',
-            },
-            {
-              value: 'last week',
-              label: 'Last week',
-            },
-            {
-              value: 'last month',
-              label: 'Last month',
-            },
-            {
-              value: 'last quarter',
-              label: 'Last quarter',
-            },
-            {
-              value: 'last year',
-              label: 'Last year',
-            },
-          ]}
-        />
-        <Select
-          placeholder="Compare to"
-          style={{
-            width: 300,
-          }}
-          options={[
-            {
-              value: 'previous period',
-              label: 'Previous period',
-            },
-            {
-              value: 'previous year',
-              label: 'Previous year',
-            },
-          ]} />
+        <div className='d-inline-block'>
+          <Paragraph>Date range:</Paragraph>
+          <Space>
+          <Select
+            placeholder="Select a date range"
+            style={{
+              width: 300,
+            }}
+            options={[
+              {
+                value: 'today',
+                label: 'Today',
+              },
+              {
+                value: 'yesterday',
+                label: 'Yesterday',
+              },
+              {
+                value: 'week to date',
+                label: 'Week to date',
+              },
+              {
+                value: 'month to date',
+                label: 'Month to date',
+              },
+              {
+                value: 'quarter to date',
+                label: 'Quarter to date',
+              },
+              {
+                value: 'year to date',
+                label: 'Year to date',
+              },
+              {
+                value: 'last week',
+                label: 'Last week',
+              },
+              {
+                value: 'last month',
+                label: 'Last month',
+              },
+              {
+                value: 'last quarter',
+                label: 'Last quarter',
+              },
+              {
+                value: 'last year',
+                label: 'Last year',
+              },
+            ]}
+          />
+          <Select
+            placeholder="Compare to"
+            style={{
+              width: 300,
+            }}
+            options={[
+              {
+                value: 'previous period',
+                label: 'Previous period',
+              },
+              {
+                value: 'previous year',
+                label: 'Previous year',
+              },
+            ]} />
+          
         </Space>
+        </div>
+        <div className='d-inline-block' style={{paddingLeft:"8px"}}>
+          <Paragraph>Custom date range:</Paragraph>
+          <RangePicker />
+        </div>
+        
+        
       </header>
       <div className='mt-4'>
           <div className='row'>
